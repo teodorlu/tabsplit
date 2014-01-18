7bf29c31fd1b8edaa0878cb24d051789ce3506c0
Teodor Heggelund <teodor.heggelund@gmail.com>
2014-01-18 17:14:09 +0100

    Reformatting, running fine so far

--- preprocessor.py
+++ preprocessor.py
@@ -7,13 +7,27 @@ def main():
 		infolist = l.strip().replace(',', '').split()
 		data.append(infolist)
 	payments = map(parsepayment, data)
-	print payments
+	moneystate = calculatemoney(payments)
+	for (person, money) in moneystate.iteritems():
+		print person + ": " + str(money)
 
 def parsepayment(payment):
 	payer = payment[0]
 	amount = payment[2]
 	leechers = payment[4:]
 	return (payer, amount, leechers)
+
+# Name of a person's amount of money, plus or minus?
+def calculatemoney(payments):
+	d = {}
+	for payment in payments:
+		payer = payment[0]
+		leechers = payment[2]
+		for person in [ payer ] + leechers:
+			if not person in d:
+				d[person] = 0
+
+	return {'Teodor': 40, "Erik": 60}
 
 if __name__ == "__main__":
 	main()
