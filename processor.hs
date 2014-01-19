597b16d1433c0a4d2dd7171386ede9a4dabb3a9f
Erik Rybakken <erikryb@stud.ntnu.no>
2014-01-19 01:42:05 +0100

    processor.hs now working. Need to fix rounding errors.

--- processor.hs
+++ processor.hs
@@ -1,16 +1,23 @@
+import Data.List
+import Data.List.Split
+
+maketwolists :: (Num a, Ord a) => [(String, a)] -> ([(String, a)], [(String, a)])
+maketwolists xs = partition (\x -> (balance x) <= 0) xs
+
 balance = snd
 person = fst
 
 findpayments :: (Num a, Ord a) => [(String, a)] -> [(String, a)] -> [(String, String, a)]
 findpayments (payer:xs) (payee:ys)
-    | (abs . balance) payer > (abs . balance) payee = first:(findpayments ((person payer, (balance payer) - amount):xs) ys)
-    | (abs . balance) payer < (abs . balance) payee = first:(findpayments xs ((person payee, (balance payee) + amount):ys))
+    | (abs . balance) payer > (abs . balance) payee = first:(findpayments ((person payer, (balance payer) + amount):xs) ys)
+    | (abs . balance) payer < (abs . balance) payee = first:(findpayments xs ((person payee, (balance payee) - amount):ys))
     | otherwise                                     = first:(findpayments xs ys)
     where
        first = (person payer, person payee, amount)
-       amount = min (balance payer) ((abs . balance) payee)
-findpayments [] [] = []
+       amount = min ((abs . balance) payer) (balance payee)
+findpayments [] _ = []
+findpayments _ [] = []
 
-
-t1 = [("Erik", 120.5), ("Teodor", 10.5)]
-t2 = [("Cecilie", -20.5), ("Kristina", -110.5)]
+main = do
+    a <- getContents
+    putStrLn $ unlines $ map (\(x,y,z) -> x ++ " -> " ++ y ++ ": " ++  show z) $ (uncurry findpayments) $ maketwolists $ map ((\(x:y:ys) -> (x, (read y :: Float))) . (splitOn ":")) (lines a)
