f681c275bb472c2ccbf909b51e2078d971b9ca21
Erik Rybakken <erikryb@stud.ntnu.no>
2014-01-31 16:00:55 +0100

    Removed redundant brackets

--- processor.hs
+++ processor.hs
@@ -2,16 +2,16 @@ import Data.List
 import Data.List.Split
 
 maketwolists :: (Num a, Ord a) => [(String, a)] -> ([(String, a)], [(String, a)])
-maketwolists xs = partition (\x -> (balance x) <= 0) xs
+maketwolists = partition (\x -> balance x <= 0)
 
 balance = snd
 person = fst
 
 findpayments :: (Num a, Ord a) => [(String, a)] -> [(String, a)] -> [(String, String, a)]
 findpayments (payer:xs) (payee:ys)
-    | - balance payer > balance payee = first:(findpayments ((person payer, (balance payer) + amount):xs) ys)
-    | - balance payer < balance payee = first:(findpayments xs ((person payee, (balance payee) - amount):ys))
-    | otherwise                                     = first:(findpayments xs ys)
+    | - balance payer > balance payee = first:findpayments ((person payer, balance payer + amount):xs) ys
+    | - balance payer < balance payee = first:findpayments xs ((person payee, balance payee - amount):ys)
+    | otherwise                                     = first:findpayments xs ys
     where
        first = (person payer, person payee, amount)
        amount = min (- balance payer) (balance payee)
@@ -20,4 +20,4 @@ findpayments _ [] = []
 
 main = do
     a <- getContents
-    putStrLn $ unlines $ map (\(x,y,z) -> x ++ " -> " ++ y ++ ": " ++  show z) $ (uncurry findpayments) $ maketwolists $ map ((\(x:y:ys) -> (x, (read y :: Float))) . (splitOn ":")) (lines a)
+    putStrLn $ unlines $ map (\(x,y,z) -> x ++ " -> " ++ y ++ ": " ++  show z) $ uncurry findpayments $ maketwolists $ map ((\(x:y:ys) -> (x, read y :: Float)) . splitOn ":") (lines a)
